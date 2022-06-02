525f70418cf3f44398a519dd50d88391bd2c2a7d
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-02 14:11:14 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const newArray=[]
+
+function writeCards(stringArray,eventName){
+    for (let i = 0; i < stringArray.length; i++) {
+        // newArray.push(stringArray[i]);
+        newArray.push(`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`);
+        debugger
+    }
+    return newArray;
+}
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function wrapGifts(gifts) {
+    let i = 0; // the initialization moves OUTSIDE the body of the loop!
+    while (i < gifts.length) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      i++; // the iteration moves INSIDE the body of the loop!
+    }
+  
+    return gifts;
+  }
+  
+
+  function countDown(count){
+      let i=count;
+      while(i>=0){
+          console.log(i);
+          i--;
+      }
+  }
+  countDown(10);
